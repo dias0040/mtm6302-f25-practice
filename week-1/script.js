573df6e08c99aad7730e625332c389b6d916ddca
Anjana Imesh Dias Samarajeewa <dias0040s@algonquinlive.com>
2025-09-08 14:00:37 -0400

    publish week-11

---
+++ week-1/script.js
@@ -0,0 +1,26 @@
+alert("dont hit ok");
+console.log("who are you?");
+
+const pet = "dragon";
+let snack = "pizza";
+
+console.log(snack);
+
+// pet = "dog";
+snack = "sushi🍣";
+
+console.log(snack);
+
+
+const quantity = '3';
+
+console.log("I want " + quantity + " " + snack + '.');
+console.log(`I want ${quantity} ${snack}.`);
+
+let age = 25;
+age = age + 5;
+console.log(age);
+
+let minage = 18;
+
+console.log('acess granted: ' + age > minage);
